783c23553b1d73362a5e8f58616ded4dfb254565
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-12-24 00:05:17 +0700

    chore: add exclude

--- src/tarball.ts
+++ src/tarball.ts
@@ -6,7 +6,7 @@ export async function tarballFolder(target: string) {
   const targetFileName = target.split("/").at(-1);
 
   await exec(
-    `tar -czf out/${targetFileName}.tar.gz --exclude=postgres-data --exclude=gitea-data/ssh --exclude=gitea-data/git/repositories/carelessdev -C ${target} .`,
+    `tar -czf out/${targetFileName}.tar.gz --exclude=postgres-data --exclude=portainer-data --exclude=gitea-data/ssh --exclude=gitea-data/git/repositories/carelessdev -C ${target} .`,
   );
 
   return performance.now() - start;
