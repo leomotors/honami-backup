f52257bff3436a38047e3a0b4dc1a18f80dfc6ce
Leomotors Bot (real) <59821765+Leomotors@users.noreply.github.com>
2023-12-27 00:14:02 +0700

    backup more data because I now have SSD

--- src/tarball.ts
+++ src/tarball.ts
@@ -6,10 +6,10 @@ export async function tarballFolder(target: string) {
   const targetFileName = target.split("/").at(-1);
 
   await exec(
-    `sudo tar -czf out/${targetFileName}.tar.gz --exclude=postgres-data --exclude=gitea-data/ssh --exclude=gitea-data/git/repositories/carelessdev -C ${target} .`,
+    `sudo tar -czf out/${targetFileName}.tar.gz --exclude=postgres-data -C ${target} .`,
   );
 
-  await exec(`sudo chown -R 1000:1003 out/${targetFileName}.tar.gz`)
+  await exec(`sudo chown -R 1000:1003 out/${targetFileName}.tar.gz`);
 
   return performance.now() - start;
 }
